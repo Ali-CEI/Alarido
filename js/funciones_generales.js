ba7a1b78f7a85f4ff25cd5f35002d9267f34d419
Ali <aliabasc@alumnos.cei.es>
2025-07-20 21:53:41 +0200

    newsletter

--- js/funciones_generales.js
+++ js/funciones_generales.js
@@ -87,7 +87,7 @@ if (!form) return;
     simulacion.style.transform = "translateY(-2.5em)";
 
   } else if (window.innerWidth < 771) {
-    form.style.transform = "translateY(-3.7em)";
+    form.style.transform = "translateY(-3.9emem)";
       simulacion.style.transform = "translateY(-1.9em)";
 
   } else {
@@ -108,7 +108,7 @@ cierre.addEventListener("mouseleave", () => {
     form.style.transform = "translateY(-2.7em)";
     simulacion.style.transform = "translateY(-0.7em)";
       } else {
-    form.style.transform = "translateY(-2.1em)";
+    form.style.transform = "translateY(-2.25em)";
     simulacion.style.transform = "translateY(-0.7em)";
     }
     
