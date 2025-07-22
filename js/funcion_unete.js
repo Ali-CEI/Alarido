47b253637e4937bd3041d27ca91e40da34ffcff1
Ali <aliabasc@alumnos.cei.es>
2025-07-22 23:35:05 +0200

    creo js unete

---
+++ js/funcion_unete.js
@@ -0,0 +1,23 @@
+    
+/*PAGINA DE UNETE*/
+//funcion que crea un rastro de puntos donde el puntero al entrar desde el ordenador
+//cambiar por estrellas o por una linea seguida
+function rastro(){
+  document.addEventListener('mousemove', rastro => {
+    const punto = document.createElement('div');
+    punto.className = 'rastro';
+    punto.style.left = rastro.clientX + 'px';
+    punto.style.top = rastro.clientY + 'px';
+
+    document.body.appendChild(punto);
+    punto.addEventListener('animationend', () => {
+      punto.remove();
+    });
+  });
+  };
+
+ document.addEventListener('DOMContentLoaded', () => {//invocar al cargar
+
+    rastro();
+
+});
